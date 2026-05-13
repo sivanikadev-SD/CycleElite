1cb916e776bf313e0cbcba0737327b8847c38997
sivanika <sivanika2025@gmail.com>
2026-05-13 12:29:52 +0530

    update

--- js/main.js
+++ js/main.js
@@ -24,9 +24,7 @@ function setDir(d) {
   document.documentElement.setAttribute('dir', d);
   if (document.body) document.body.setAttribute('dir', d);
   document.querySelectorAll('[data-rtl-icon]').forEach(el => {
-    el.innerHTML = d === 'rtl'
-      ? `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="17" y1="10" x2="3" y2="10"/><line x1="21" y1="6" x2="3" y2="6"/><line x1="21" y1="14" x2="3" y2="14"/><line x1="17" y1="18" x2="3" y2="18"/></svg>`
-      : `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="21" y1="10" x2="7" y2="10"/><line x1="21" y1="6" x2="3" y2="6"/><line x1="21" y1="14" x2="3" y2="14"/><line x1="21" y1="18" x2="7" y2="18"/></svg>`;
+    el.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 17H4"/><path d="m10 23-6-6 6-6"/><path d="M4 7h16"/><path d="m14 1 6 6-6 6"/></svg>`;
   });
 }
 
